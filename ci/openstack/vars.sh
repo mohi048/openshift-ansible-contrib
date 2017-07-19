f04c06803073d4a77109b0a618630ae4424678e2
Tomas Sedovic <tomas@sedovic.cz>
2017-07-19 17:54:44 +0200

    Oh right we can't have empty if blocks in bash

--- ci/openstack/vars.sh
+++ ci/openstack/vars.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ "${RUN_OPENSTACK_CI:-}" == true ]; then
+if [ "${RUN_OPENSTACK_CI:-}" == true ]
+then
     # TODO(shadower): check that the commit changed roles or playbooks/provisioning
     # TODO(shadower): check that a project admin asked for the CI to run
+    RUN_OPENSTACK_CI=true
 fi
 
 export RUN_OPENSTACK_CI
